d302e176ae5a38af270745a4dc1ebdde22d5e140
jess <jessiasato@gmail.com>
2021-11-10 12:56:39 -0300

    creacion de html tienda + js

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
+//Mensaje de bienvenida
 let nombreIngresado = prompt ("Ingresar su nombre");
 alert ("Bienvenid@ " + nombreIngresado);
+
+
+//Agregar un producto al carrito
+let productoAgregado = "añadir producto al carrito"
+if (productoAgregado = true){
+    alert ("Producto añadido correctamente");
+}
+
+//Cálculo básico - Sumatoria dos productos en el carrito
+let cookies == 100;
+let cupcake == 220;
+const suma = function (a,b) {
+    return parseInt (a + b);
+};
+console.log (suma(cookies,cupcake);
+
+//Calculo sumatoria productos del carrito
+let cookies == 100;
+let cupcake == 220;
+let bombones == 315;
+const totalCarrito = function (producto1,producto2,producto3,) {
+    return parseInt (producto1 + producto2 + producto3);
+};
+alert ("El total de su carrito es " + suma(cookies,cupcake,bombones));
+
+//Calculo de una compra en 3 cuotas fijas
+let totalCarrito = 1200;
+const division = function (a,b) {
+    return parseInt (a / b);
+}
+console.log (division(sumatoriaCarrito,6));
