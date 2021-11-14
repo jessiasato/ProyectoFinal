8acb57afe5f43f76466c56dcfef90c918feb1aab
jess <jessiasato@gmail.com>
2021-11-14 12:19:40 -0300

    ajuste js 14.11

--- script.js
+++ script.js
@@ -5,30 +5,28 @@ alert ("Bienvenid@ " + nombreIngresado);
 
 //Agregar un producto al carrito
 let productoAgregado = "añadir producto al carrito"
-if (productoAgregado = true){
+if (productoAgregado === true){
     alert ("Producto añadido correctamente");
 }
 
 //Cálculo básico - Sumatoria dos productos en el carrito
-let cookies == 100;
-let cupcake == 220;
+let cookies = 100;
+let cupcake = 220;
 const suma = function (a,b) {
-    return parseInt (a + b);
+    return (a + b);
 };
-console.log (suma(cookies,cupcake);
+console.log (suma(cookies,cupcake));
 
 //Calculo sumatoria productos del carrito
-let cookies == 100;
-let cupcake == 220;
-let bombones == 315;
+let bombones = 315;
 const totalCarrito = function (producto1,producto2,producto3,) {
-    return parseInt (producto1 + producto2 + producto3);
+    return (producto1 + producto2 + producto3);
 };
-alert ("El total de su carrito es " + suma(cookies,cupcake,bombones));
+alert ("El total de su carrito es " + totalCarrito(cookies,cupcake,bombones));
 
 //Calculo de una compra en 3 cuotas fijas
-let totalCarrito = 1200;
+let totalPago = 1200;
 const division = function (a,b) {
-    return parseInt (a / b);
+    return (a / b);
 }
-console.log (division(sumatoriaCarrito,6));
+console.log (division(totalPago,6));
